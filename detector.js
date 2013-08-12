e74cc282f5e33b623b1f39eac4f0c4b6cb0a13db
ashiguruma <tom.loudon@axisto.com>
2013-08-12 13:48:22 +0100

    Added a simple JavaScript object to spcify what should be detected. Added an index.html file to run the script

--- detector.js
+++ detector.js
@@ -9,9 +9,41 @@
 'use strict';
 
 function Detector() {
-  
+  this.data = [];
 }
 
-Detector.prototype.sendData = function () {
-  
+Detector.prototype.collect = function (property, collector) {
+  if (property === undefined || typeof property === 'number') {
+    throw 'Properties must be a string property name or an array of property names';
+  }
+
+  if (Object.prototype.toString.call(property) === '[object Array]') {
+    for (var i = 0; i < property.length; i++) {
+      this.data[property[i]] = this[this.getMethodByProperty(property[i])]();
+    }
+  } else if (typeof property === 'string' && collector !== undefined && typeof collector === 'function') {
+    this.data[property] = collector();
+  } else {
+    this.data[property] = (this[this.getMethodByProperty(property)]());
+  }
+
+  return this;
 };
+
+Detector.prototype.getMethodByProperty = function (method) {
+  return 'get' + method.charAt(0).toUpperCase() + method.substr(1).toLowerCase();
+};
+
+Detector.prototype.getBrowser = function () {
+  return navigator.userAgent;
+};
+
+Detector.prototype.getFlash = function () {
+  if (window.swfobject === undefined) {
+    throw 'SWFObject is not available. SWFObject is required for Adobe Flash detection.';
+  } else {
+    return window.swfobject.getFlashPlayerVersion();
+  }
+};
+
+
