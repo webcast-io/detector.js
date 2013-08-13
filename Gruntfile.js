b33d474bdf8f8b4bc97480c2484da5ec013b6165
ashiguruma <tom.loudon@axisto.com>
2013-08-13 15:58:33 +0100

    Added html and inline js linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,18 +10,28 @@ module.exports = function (grunt) {
       all: ['Gruntfile.js', 'detector.js']
     },
 
-    mocha: {
-      src: ['test/index.html'],
-      options: {
-        run: true,
-        reporter: 'Spec'
-      }
+    inlinelint: {
+      html: ['index.html']
+    },
+
+    htmllint: {
+      all: ['index.html']
     }
+
+    // mocha: {
+    //   src: ['test/index.html'],
+    //   options: {
+    //     run: true,
+    //     reporter: 'Spec'
+    //   }
+    // }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-lint-inline');
+  grunt.loadNpmTasks('grunt-html');
   grunt.loadNpmTasks('grunt-mocha');
 
-  grunt.registerTask('default', ['jshint', 'mocha']);
+  grunt.registerTask('default', ['jshint', 'inlinelint', 'htmllint']);
 };
